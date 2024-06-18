e30951748cf06978a4483103ab6a507dbeaae189
iyk_faiz <ikechukwugodwin22@yahoo.com>
2024-06-18 11:56:03 +0100

    project init

---
+++ data/constant.tsx
@@ -0,0 +1,52 @@
+import number1 from "@/public/icons/number-1.png";
+import number2 from "@/public/icons/number-2.png";
+import number3 from "@/public/icons/number-3.png";
+import number4 from "@/public/icons/number-4.png";
+import number5 from "@/public/icons/number-5.png";
+
+import salebar from "@/public/icons/salesbar.png";
+import driving from "@/public/icons/driving.png";
+import globe from "@/public/icons/globe.png";
+import businessMan from "@/public/icons/businessman.png";
+import cashAtHand from "@/public/icons/cash-at-hand.png";
+
+
+export const Data = [
+    {
+        number: number1,
+        icon: salebar,
+        title: "To increase sales",
+        messagge: "Having a website gives you more visibility and makes you more accesible which increases you sales drastically "
+    },
+    {
+        number: number2,
+        icon: driving,
+        title: "To Gain Control",
+        messagge: "To have your own platform that you own 100% (Instagram is not yours. your account can be deactivated anytime)Take control of your business today"
+    },
+    {
+        number: number3,
+        icon: globe,
+        title: "To sell Globally",
+        messagge: "Getting a website opens the door to your business worldwide clients all over the world are more comfortable buying via a website"
+    },
+    {
+        number: number4,
+        icon: cashAtHand,
+        title: "To Keep the Sales Going",
+        messagge: "Owning a website helps you keep the sales going 24/7 no going back and forth in DM, wasting valuable time and energy"
+    },
+    {
+        number: number5,
+        icon: businessMan,
+        title: "To improve credibility",
+        messagge: "Having a fully functional website improves your credibility to your potential clients and avoids any raise of eyebrowns"
+    },
+]
+
+export const uniqueService = [
+    { title: "Seamless Payment", message: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
+    { title: "Hotel Reservation", message: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
+    { title: "Flight Booking", message: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
+    { title: "QR Pay", message: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
+]
